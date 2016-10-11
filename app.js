a04bf4c61a652053052c9414515bd7cc41686bb7
Kaitlin Coventry-Cox <kaitlincov@gmail.com>
2016-10-11 10:11:38 -0400

    learned how to take in user input

--- app.js
+++ app.js
@@ -2,16 +2,29 @@ var express = require("express");
 var app     = express();
 var path    = require("path");
 var serveStatic = require('serve-static');
-app.use(express.static('public'));
+var nunjucks = require('nunjucks');
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use('/static/css', express.static(__dirname+'/static/css'));
+app.use('/static/js', express.static(__dirname+'/static/js'));
+app.use('/static/img', express.static(__dirname+'/static/img'));
 
-app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
 });
 
-app.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname+'/about.html'));
+app.get('/',function(req, res){
+  res.render('index.html');
+  //this will hopefully render index.html.
+});
+
+app.post('/core',function(req,res){
+    console.log('Core: ' + req.body.core);
+    var core = req.body.core;
+    res.render('classes.html', {core: core});
 });
 
 app.get('/projects',function(req,res){
