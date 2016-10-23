5cc40bb65acc4a4e137b45698b1f4f080cb849cb
Kaitlin Coventry-Cox <kaitlincov@gmail.com>
2016-10-23 10:18:37 -0400

    HackRU Progress - select core code, displays from rutgers api

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path    = require("path");
 var serveStatic = require('serve-static');
 var nunjucks = require('nunjucks');
 var bodyParser = require('body-parser');
+var request = require('request');
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -17,14 +18,39 @@ nunjucks.configure('views', {
 });
 
 app.get('/',function(req, res){
-  res.render('index.html');
-  //this will hopefully render index.html.
+    var url = "http://sis.rutgers.edu/soc/init.json"; //core code api
+
+    //function that takes all the core code entries for nb and pulls just the actual code and transfers to an array
+    var callbackone = function (error, response, body){
+        var coreCodes = [];
+        if (!error && response.statusCode === 200) {
+            var bodyCodes = body['coreCodes'];
+            for(i=0; i<bodyCodes.length; i++){
+                if(bodyCodes[i].campus=='NB'){
+                    coreCodes.push(bodyCodes[i].code);
+                }
+            }   
+            res.render('index.html', {'coreCodes':coreCodes});
+        }   
+    }
+
+    //request is asynchronous: when it completes it calls callbackone
+    request ({ url: url, json: true}, callbackone); 
 });
 
 app.post('/core',function(req,res){
-    console.log('Core: ' + req.body.core);
     var core = req.body.core;
-    res.render('classes.html', {core: core});
+
+    var url = "http://sis.rutgers.edu/soc/coreCodeCourses.json?semester=92016&campus=NB&level=U&coreCode="+core;
+
+    var callbacktwo = function(error, response, body){
+        if (!error && response.statusCode === 200){
+            var courses = body; //gives us access to entire courses document based on core code
+        }    
+    res.render('classes.html', {core: core, courses: courses});
+    }
+
+    request ({ url: url, json: true}, callbacktwo); 
 });
 
 app.get('/projects',function(req,res){
